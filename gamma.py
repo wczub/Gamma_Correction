b0cda95b499eaa5ee67b18e444fce08c6bf9c1a3
wczub <willczubiak@gmail.com>
2015-02-25 15:48:18 -0800

    Started the program, added header and first import from PIL

--- gamma.py
+++ gamma.py
@@ -1,1 +1,11 @@
-#this is my programm
+"""
+# Project: Gamma Normalization
+# Filename: gamma.py
+# Description:  This program will take in a video from the user
+# 				and then it will normalize the gamma levels 
+#				so it is never too bright or too dark.
+"""
+
+ 
+from PIL import Image
+
