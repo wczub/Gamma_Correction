4fbfef8e76fc23bdd59564509a3a98c0267b3890
wczub <willczubiak@gmail.com>
2015-03-09 15:35:53 -0700

    Added the ability to either darken or brighten depending on average values of the pixels in the picture. Still need to create better algorithm for how much to brighten or darken.

--- gamma.py
+++ gamma.py
@@ -1,73 +1,92 @@
-###################################################################
-# Project: Gamma Normalization                                    #
-# Filename: gamma.py                                              #
-# Description:  This program will take in a video from the user   #
-# 				and then it will normalize the gamma levels       #
-#				so it is never too bright or too dark.            #
-##################################################################
+#==================================================================
+# Project: Gamma Normalization                                    
+# Filename: gamma.py                                              
+# Description:  This program will take in a video from the user   
+#               and then it will normalize the gamma levels       
+#               so it is never too bright or too dark.            
+#==================================================================
  
 
  
 from PIL import Image
 from PIL import ImageEnhance
 
-def get_file():
+def get_file(): #Simple gets the file for testing with single photo
     path = raw_input("Enter file Path: ")
-    return path
+    pic = Image.open(path, 'r')
+    return pic
 #END DEF
     
-def get_avg_pixel(pic):    
-    pixel = pic.getdata()
+def get_avg_pixel(pic): 
+    #this function returns the average value of all the
+    #pixels in the image
+    pixel = list(pic.getdata()) 
     avg = 0.0
-    for y in range(0,pic.height()):
-        for x in range(0,pic.width()):
-            pixRGB = pic.getpixel(x,y)
+    width, height = pic.size 
+    #loops through the photo, checking the every 10th pixel, so as to 
+    #speed up the process 
+    for y in range(0, height-1, 5):
+        for x in range(0, width-1, 5):
+            pixRGB = pic.getpixel((x, y))
             R,G,B = pixRGB
-            avg += (sum([R,G,B]) / 3.0)
-    avg = avg / (pic.width() * pic.height())
+            avg += (sum([R,G,B]) / 3.0) #gets the average rgb value of pixels
+    avg = avg / ((width/5) * (height/5)) 
     return avg
 #END DEF
 
-def change_brightness(filename):
+def change_brightness(org_image):
     #function changes brightness depending on the average pixel value
     
     #load the original image into a list
-    org_image = Image.open(filename, 'r')
+    #org_image = Image.open(filename, 'r')
     pixels = org_image.getdata()
-    
+    extent = 0.0
     pic_brightness = get_avg_pixel(org_image)
+    print pic_brightness
     action = 'darken'
-    if(pic_brightness > 50):
+    extent += (pic_brightness / 100)
+    if(pic_brightness < 50):
         action = 'lighten'
     
+    #if the photo is too bright it won't darken it too much
+    #and if the photo is too dark it won't brighten it too much
+    print extent
+    if extent > .40:
+        extent = .40
+    elif extent < .10:
+        extent = .10
+    print extent
 
-    #initialize the new image
+    #creates a new image to preserve the original
     new_image = Image.new('RGB', org_image.size)
     new_image_list = []
 
     brightness_multiplier = 1.0 
     if action == 'lighten':
-        brightness_multiplier += (extent/100.0)
+        brightness_multiplier += (extent)
     else:
-        brightness_multiplier -= (extent/100.0)
+        brightness_multiplier -= (extent)
 
-    #for each pixel, append the brightened or darkened version to the new image list
+    #goes through and adds the changed pixels into an array
+    #for the new image
     for pixel in pixels:
         new_pixel = (int(pixel[0] * brightness_multiplier),
                      int(pixel[1] * brightness_multiplier),
                      int(pixel[2] * brightness_multiplier))
-        #check the new pixel values are within rgb range
+        #checks to make sure every pixel is still in rgb range
         for pixel in new_pixel:
             if pixel > 255:
                 pixel = 255
             elif pixel < 0:
                 pixel = 0
-
+        #Once the pixel is an acceptable pixel, places it in an array
+        #to create the new image
         new_image_list.append(new_pixel)
 
-    #save the new image
+    #uses the array of pixels to create the new image.
     new_image.putdata(new_image_list)
     new_image.save("/media/removable/Storage/Photos/brightmine.jpg")
+#END DEF
 
-path = get_file()
-change_brightness(path)
+photo = get_file()
+change_brightness(photo)
